b35027378e6be125b4de4012b8cfc15de2dd548c
Jason Ng <jasonngjh@gmail.com>
2016-11-23 10:32:41 +0800

    added zombie class

---
+++ zombie.cpp
@@ -0,0 +1,37 @@
+#include "zombie.h"
+
+//=============================================================================
+// default constructor
+//=============================================================================
+Zombie::Zombie() : Entity()
+{
+	spriteData.width = ZombieNS::WIDTH;
+	spriteData.height = ZombieNS::HEIGHT;
+	spriteData.x = ZombieNS::X;
+	spriteData.y = ZombieNS::Y;
+	spriteData.rect.bottom = ZombieNS::HEIGHT;
+	spriteData.rect.right = ZombieNS::WIDTH;
+	velocity.x = 0;                             // velocity X
+	velocity.y = 0;                             // velocity Y
+	frameDelay = ZombieNS::ZOMBIE_ANIMATION_DELAY;
+	startFrame = ZombieNS::ZOMBIE_START_FRAME;      // first frame of ship animation
+	endFrame = ZombieNS::ZOMBIE_END_FRAME;      // last frame of ship animation
+	currentFrame = startFrame;
+	radius = ZombieNS::WIDTH / 2.0;
+	collisionType = entityNS::CIRCLE;
+}
+
+//=============================================================================
+// update
+// typically called once per frame
+// frameTime is used to regulate the speed of movement and animation
+//=============================================================================
+void Zombie::update(float frameTime){
+	Entity::update(frameTime);
+
+}
+
+void Zombie::movement(){
+	int zombieX = spriteData.x;
+
+}
---
+++ zombie.h
@@ -0,0 +1,32 @@
+#ifndef _ZOMBIE_H                 // Prevent multiple definitions if this 
+#define _ZOMBIE_H               // file is included in more than one place
+#define WIN32_LEAN_AND_MEAN
+
+#include "entity.h"
+#include "constants.h"
+
+namespace ZombieNS{
+	//zombie height and width
+	const int ZOMBIE_COLS = 2;
+	const int HEIGHT = 32;
+	const int WIDTH = 32;
+	const int  ZOMBIE_START_FRAME = 0;         // starting frame of ship animation
+	const int  ZOMBIE_END_FRAME = 3;           // last frame of ship animation
+	const float ZOMBIE_ANIMATION_DELAY = 0.15f;
+	const int X = GAME_WIDTH / 2 - WIDTH / 2;   // location on screen
+	const int Y = GAME_HEIGHT / 2 - HEIGHT / 2;
+}
+
+class Zombie : public Entity{
+
+public:
+	//constructor
+	Zombie();
+
+	// inherited member functions
+	void update(float frameTime);
+	
+	void movement();
+};
+
+#endif
